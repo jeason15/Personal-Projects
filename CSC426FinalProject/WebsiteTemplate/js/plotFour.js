88d2e41757b7472394805e3c511efe4c653b8e41
Joshua Eason <jeason15@gmail.com>
2022-05-07 18:14:55 -0500

    added more content and redid pie chart

--- CSC426FinalProject/WebsiteTemplate/js/plotFour.js
+++ CSC426FinalProject/WebsiteTemplate/js/plotFour.js
@@ -10,6 +10,8 @@ new Chart(document.getElementById("plotFour"), {
         datasets: [
             {
                 label: "Number of Models",
+                borderColor: ['rgba(235,10,30)', 'rgba(19,81,216)', 'rgba(209,173,87)', 'rgba(204,0,0)',
+                'rgba(0,0,0)', 'rgba(72,95,43)', 'rgba(0,48,130)', 'rgba(126,128,131)', 'rgba(151,38,38)', 'rgba(0,72,144)'],
                 backgroundColor: ['rgba(235,10,30)', 'rgba(19,81,216)', 'rgba(209,173,87)', 'rgba(204,0,0)',
                     'rgba(0,0,0)', 'rgba(72,95,43)', 'rgba(0,48,130)', 'rgba(126,128,131)', 'rgba(151,38,38)', 'rgba(0,72,144)'],
                 data: [1933099, 1804824, 1422031, 1308476, 919090, 768713, 726715, 677494, 647329, 598480]
@@ -17,6 +19,7 @@ new Chart(document.getElementById("plotFour"), {
         ]
     },
     options: {
+        hoverOffset: 60,
         title: {
             display: true,
             text: "Vehicle Sales 2022 by Manufacturer (top 10)"
